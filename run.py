9aa89a26d902c55612db68a238578fa73c43a938
ahmed-amro <66771265+ahmed-amro@users.noreply.github.com>
2021-04-12 16:28:17 +0200

    Create run.py

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import Python/test.py
+
+def RunFromRoot():
+    print ("This is the main launcher for DigitalAPSReplica")
+
+if __name__ == '__main__':
+    # service.py executed as script
+    # do something
+    RunFromRoot()
+    test.some_func()
