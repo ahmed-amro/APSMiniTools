e0bea1a39f748ab3f12d40ae2a81690271c0eaf4
ahmed-amro <66771265+ahmed-amro@users.noreply.github.com>
2021-04-12 16:28:53 +0200

    Update test.py

--- Python/test.py
+++ Python/test.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 
-print("I'm alive")
+
+def some_func():
+    print("I'm alive test")
+
+if __name__ == '__main__':
+    # test1.py executed as script
+    # do something
+    some_func()
+    
+
